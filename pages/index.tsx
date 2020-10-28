60d6eb8b55ab8c082c51793bb9b5f29204b7d60b
Maximous Black <maximousblk@gmail.com>
2020-10-28 06:24:21 +0000

    minor changes and cleanup

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,28 +26,30 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing <code className={styles.code}>pages/index.js</code>
+          Get started by editing <code className={styles.code}>pages/api/auth/[...nextauth].ts</code>
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+          <a href="/api/auth/session" className={styles.card}>
+            <h3>Session &rarr;</h3>
+            <p>The contents of the session object.</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+          <a href="/api/auth/csrf" className={styles.card}>
+            <h3>CSRF token &rarr;</h3>
+            <p>
+              NextAuth CSFR Token.
+            </p>
           </a>
 
-          <a href="https://github.com/vercel/next.js/tree/master/examples" className={styles.card}>
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+          <a href="/api/auth/signin" className={styles.card}>
+            <h3>LogIn &rarr;</h3>
+            <p>Login usig one of the providers.</p>
           </a>
 
-          <a href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app" className={styles.card}>
-            <h3>Deploy &rarr;</h3>
-            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
+          <a href="/api/auth/signin" className={styles.card}>
+            <h3>LogOut &rarr;</h3>
+            <p>Log out of the current session</p>
           </a>
         </div>
       </main>
