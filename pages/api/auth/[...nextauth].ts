edbf8004e33790a254fc4f239d3281de12e02aae
Maximous Black <maximousblk@gmail.com>
2020-10-28 05:57:37 +0000

    test

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,14 @@
+import NextAuth from 'next-auth';
+import Providers from 'next-auth/providers';
+
+const options = {
+  // Configure one or more authentication providers
+  providers: [
+    Providers.GitHub({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+  ],
+};
+
+export default (req, res) => NextAuth(req, res, options);
